d200e7ee6cd43cba1d0dbaee827ea19a863ef438
ykchahine <42751366+ykchahine@users.noreply.github.com>
2021-10-16 16:34:15 -0700

    Update english.js

--- jss/english.js
+++ jss/english.js
@@ -1,11 +1,34 @@
-function check() {
-    var c = 0;
-    var message;
-    var message2;
-    message = 'Correct!';
-    message2 = 'Incorrect! Try Again!'
-    var q1=document.englishtemp.question1.value;
-    if (q1="Washing Machine") {c++;}
-    if (c==1) {document.write(message);}
-    else {document.write(message2)}
-}
+var submitAnswer = function() {
+
+    var radios = document.getElementsByName('question1');
+    var val= "";
+    for (var i = 0, length = radios.length; i < length; i++) {
+        if (radios[i].checked) {
+           val = radios[i].value; 
+           break;
+         }
+    }
+if ( val == "Flower" ) {
+      alert('Answer is correct!');
+    } else {
+      alert('Answer is wrong');
+    }
+  };
+
+var submitAnswer_Q2 = function() {
+
+    var radios = document.getElementsByName('question2');
+    var val= "";
+    for (var i = 0, length = radios.length; i < length; i++) {
+        if (radios[i].checked) {
+           val = radios[i].value; 
+           break;
+         }
+    }
+    
+if ( val == "Sleeping" ) {
+      alert('Answer is correct!');
+    } else {
+      alert('Answer is wrong');
+    }
+  };
