a2bcae2c459ac557aa9701c5c484ca96f0f399ff
ykchahine <42751366+ykchahine@users.noreply.github.com>
2021-10-18 17:30:41 -0700

    Added 4 question validation functions

--- jss/english.js
+++ jss/english.js
@@ -1,4 +1,4 @@
-var submitAnswer = function() {
+var submitAnswer_Q1 = function() {
 
     var radios = document.getElementsByName('question1');
     var val= "";
@@ -27,6 +27,42 @@ var submitAnswer_Q2 = function() {
     }
     
 if ( val == "Sleeping" ) {
+      alert('Answer is correct!');
+    } else {
+      alert('Answer is wrong');
+    }
+  };
+
+var submitAnswer_Q3 = function() {
+
+    var radios = document.getElementsByName('question3');
+    var val= "";
+    for (var i = 0, length = radios.length; i < length; i++) {
+        if (radios[i].checked) {
+           val = radios[i].value; 
+           break;
+         }
+    }
+    
+if ( val == "Thirsty" ) {
+      alert('Answer is correct!');
+    } else {
+      alert('Answer is wrong');
+    }
+  };
+
+  var submitAnswer_Q4 = function() {
+
+    var radios = document.getElementsByName('question4');
+    var val= "";
+    for (var i = 0, length = radios.length; i < length; i++) {
+        if (radios[i].checked) {
+           val = radios[i].value;
+           break;
+         }
+    }
+    
+if ( val == "Shovel" ) {
       alert('Answer is correct!');
     } else {
       alert('Answer is wrong');
