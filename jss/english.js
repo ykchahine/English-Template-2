bc5871f78d06ef981917d31454153186b09e9915
ykchahine <42751366+ykchahine@users.noreply.github.com>
2021-11-04 16:05:37 -0700

    Added sound queries and inline messages

--- jss/english.js
+++ jss/english.js
@@ -1,3 +1,4 @@
+
 var submitAnswer_Q1 = function() {
 
     var radios = document.getElementsByName('question1');
@@ -8,12 +9,23 @@ var submitAnswer_Q1 = function() {
            break;
          }
     }
-if ( val == "Flower" ) {
-      alert('Answer is correct!');
-    } else {
-      alert('Answer is wrong');
-    }
-  };
+    if ( val == "Flower" ) {
+      document.getElementById("QUESTION-VALIDATE-Q1").innerHTML = "Correct! Awesome job!";
+      var audio = document.getElementById("audio_correct");
+      audio.play();
+
+      } 
+    else if (val == "") {
+      document.getElementById("QUESTION-NONE-Q1").innerHTML = "Oops! Please choose an answer!";
+      var audio = document.getElementById("audio_none");
+      audio.play();
+        } 
+    else {
+      document.getElementById("QUESTION-VALIDATE-WRONG-Q1").innerHTML = "Incorrect! Try again!";
+      var audio = document.getElementById("audio_incorrect");
+      audio.play();
+        }
+};
 
 var submitAnswer_Q2 = function() {
 
@@ -25,13 +37,22 @@ var submitAnswer_Q2 = function() {
            break;
          }
     }
-    
-if ( val == "Sleeping" ) {
-      alert('Answer is correct!');
-    } else {
-      alert('Answer is wrong');
+    if ( val == "Sleeping" ) {
+      document.getElementById("QUESTION-VALIDATE-Q2").innerHTML = "Correct! Awesome job!";
+      var audio = document.getElementById("audio_correct");
+      audio.play();
+  } 
+    else if (val == "" ) {
+      document.getElementById("QUESTION-NONE-Q2").innerHTML = "Oops! Please choose an answer!";
+      var audio = document.getElementById("audio_none");
+      audio.play();
+    } 
+    else {
+      document.getElementById("QUESTION-VALIDATE-Wrong-Q2").innerHTML = "Incorrect! Try again!";
+      var audio = document.getElementById("audio_incorrect");
+      audio.play();
     }
-  };
+};
 
 var submitAnswer_Q3 = function() {
 
@@ -44,12 +65,22 @@ var submitAnswer_Q3 = function() {
          }
     }
     
-if ( val == "Thirsty" ) {
-      alert('Answer is correct!');
-    } else {
-      alert('Answer is wrong');
+    if ( val == "Thirsty" ) {
+      document.getElementById("QUESTION-VALIDATE-Q3").innerHTML = "Correct! Awesome job!";
+      var audio = document.getElementById("audio_correct");
+      audio.play();
+    } 
+    else if (val == "" ){
+      document.getElementById("QUESTION-NONE-Q3").innerHTML = "Oops! Please choose an answer!";
+      var audio = document.getElementById("audio_none");
+      audio.play();
+    } 
+    else {
+      document.getElementById("QUESTION-VALIDATE-Wrong-Q3").innerHTML = "Incorrect! Try again!";
+      var audio = document.getElementById("audio_incorrect");
+      audio.play();
     }
-  };
+};
 
   var submitAnswer_Q4 = function() {
 
@@ -62,9 +93,32 @@ if ( val == "Thirsty" ) {
          }
     }
     
-if ( val == "Shovel" ) {
-      alert('Answer is correct!');
-    } else {
-      alert('Answer is wrong');
+    if ( val == "Shovel" ) {
+      document.getElementById("QUESTION-VALIDATE-Q4").innerHTML = "Correct! Awesome job!";
+      var audio = document.getElementById("audio_correct");
+      audio.play();
+    } 
+    else if (val == "" ) {
+      document.getElementById("QUESTION-NONE-Q4").innerHTML = "Oops! Please choose an answer!";
+      var audio = document.getElementById("audio_none");
+      audio.play();
+    } 
+    else {
+      document.getElementById("QUESTION-VALIDATE-Wrong-Q4").innerHTML = "Incorrect! Try again!";
+      var audio = document.getElementById("audio_incorrect");
+      audio.play();
+
     }
-  };
+};
+
+//function submitForm() {
+  // Get the first form with the name
+  // Usually the form name is not repeated
+  // but duplicate names are possible in HTML
+  // Therefore to work around the issue, enforce the correct index
+ // var frm = document.getElementsByName('word-Q1')[0];
+ //   frm.submit(); // Submit the form
+//    frm.reset();  // Reset all form data
+//return false; // Prevent page refresh
+// }
+  
